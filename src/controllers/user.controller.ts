3e2de611b65c0dd7559cfbad0bb58d8be390ff34
Ritik <160255700+gritik418@users.noreply.github.com>
2026-09-12 00:33:03 +0530

    feat: add get developers endpoint

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,2 +1,43 @@
+import type { Request, Response } from "express";
+import raiseServerError from "../helpers/raise-server-error.js";
+import { UserRole } from "../generated/prisma/enums.js";
+import prisma from "../db/prisma.js";
 
+export const getDevelopers = async (req: Request, res: Response) => {
+  try {
+    const userId = req.user?.id;
+    const userRole = req.user?.role;
 
+    if (!userId || !userRole)
+      return res.status(401).json({
+        success: false,
+        message: "Unauthorized.",
+      });
+
+    if (userRole !== UserRole.ADMIN && userRole !== UserRole.PROJECT_MANAGER) {
+      return res.status(403).json({
+        success: false,
+        message: "You do not have permission to list developers.",
+      });
+    }
+
+    const developers = await prisma.user.findMany({
+      where: {
+        role: UserRole.DEVELOPER,
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+      },
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "Developers fetched successfully.",
+      data: { developers },
+    });
+  } catch (error) {
+    return raiseServerError(res, error);
+  }
+};
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,5 +1,16 @@
 import { Router } from "express";
+import authMiddleware from "../middlewares/auth.middleware.js";
+import authorize from "../middlewares/authorize.middleware.js";
+import { UserRole } from "../generated/prisma/enums.js";
+import { getDevelopers } from "../controllers/user.controller.js";
 
 const router = Router();
+
+router.get(
+  "/developers",
+  authMiddleware,
+  authorize(UserRole.ADMIN, UserRole.PROJECT_MANAGER),
+  getDevelopers,
+);
 
 export default router;
