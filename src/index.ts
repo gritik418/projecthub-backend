843866c9449cfabbda566dc674d12df16478f331
Ritik <160255700+gritik418@users.noreply.github.com>
2026-09-12 12:47:22 +0530

    feat: add active users feature

--- src/index.ts
+++ src/index.ts
@@ -6,16 +6,21 @@ import clientRoutes from "./routes/client.routes.js";
 import taskRoutes from "./routes/task.routes.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
-const app = express();
-const port = process.env.PORT || 8000;
+import http from "http";
 
 import { startTaskJobs } from "./jobs/task.job.js";
 import "./workers/task.worker.js";
+import socketServer from "./socket/socket.server.js";
+
+const app = express();
+const server = http.createServer(app);
+const port = process.env.PORT || 8000;
+
+socketServer(server);
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [process.env.FRONTEND_URL!],
     credentials: true,
   }),
 );
@@ -30,6 +35,6 @@ app.use("/api/task", taskRoutes);
 
 await startTaskJobs();
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`App served at port: ${port}`);
 });
---
+++ src/socket/socket.server.ts
@@ -0,0 +1,92 @@
+import { Server, type Socket } from "socket.io";
+import http from "http";
+
+import jwt from "jsonwebtoken";
+import type { UserRole } from "../generated/prisma/enums.js";
+
+export const ConnectedUsers = new Map<string, Socket>();
+
+const socketServer = (
+  httpServer: http.Server<
+    typeof http.IncomingMessage,
+    typeof http.ServerResponse
+  >,
+) => {
+  const io = new Server(httpServer, {
+    cors: {
+      origin: process.env.FRONTEND_URL,
+      credentials: true,
+    },
+    transports: ["websocket"],
+    allowUpgrades: true,
+    pingTimeout: 20000,
+  });
+
+  io.use((socket: Socket, next) => {
+    try {
+      const authorization = socket.handshake.auth.token;
+
+      console.log("first", authorization);
+
+      if (!authorization?.startsWith("Bearer ")) {
+        return next(new Error("Unauthorized"));
+      }
+
+      const accessToken = authorization.split(" ")[1];
+      console.log("accessToken", accessToken);
+
+      if (!accessToken) {
+        return next(new Error("Unauthorized"));
+      }
+
+      const decoded = jwt.verify(accessToken, process.env.JWT_SECRET!) as {
+        id: string;
+        email: string;
+        role: UserRole;
+      };
+
+      if (!decoded.id || !decoded.email || !decoded.role) {
+        return next(new Error("Unauthorized"));
+      }
+
+      socket.data.user = {
+        id: decoded.id,
+        email: decoded.email,
+        role: decoded.role,
+      };
+
+      next();
+    } catch (error) {
+      next(new Error("Unauthorized"));
+    }
+  });
+
+  io.on("connection", (socket) => {
+    const user = socket.data.user;
+
+    console.log("connected", user.email);
+
+    ConnectedUsers.set(user.id, socket);
+
+    if (user.role === "ADMIN") {
+      socket.join("admins");
+    }
+
+    emitActiveUsers(io);
+
+    socket.on("disconnect", () => {
+      console.log("disconnected", user.email);
+      ConnectedUsers.delete(user.id);
+
+      emitActiveUsers(io);
+    });
+  });
+};
+
+const emitActiveUsers = (io: Server) => {
+  const users = Array.from(ConnectedUsers.keys());
+
+  io.to("admins").emit("active-users", users);
+};
+
+export default socketServer;
